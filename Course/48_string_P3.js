8515e0d932bc686199db024dc5ad8db72c309396
Talismar Fernandes Costa <talismar788.una@gmail.com>
2022-04-04 19:18:12 -0300

    Shipping 88-90 and files organization

--- Course/48_string_P3.js
+++ Course/48_string_P3.js
@@ -1,35 +1,32 @@
-
-
-
 var texto = "Talismar De de Fernandes Costa talismar"
-var txt2="Talismar";
+var txt2 = "Talismar";
 
 //indexOf retorna a posição do determinado caracter
-var res=texto.indexOf("t");
+var res = texto.indexOf("t");
 console.log(res);
 
 //Encontrar o ultimo caracter encontrado
-var res=texto.lastIndexOf("e");
+var res = texto.lastIndexOf("e");
 //console.log(res);
 
 //Comparação de duas strings sao iguais ou não - 0 = iguais e != 0 são diferentes
-var res= texto.localeCompare(txt2);
+var res = texto.localeCompare(txt2);
 console.log(res);
 
 //Corta a string em um texto especifico
-var res=texto.slice(0, 8);
+var res = texto.slice(0, 8);
 //console.log(res);
 
 //Pica toda a nossa string no carater que indicar, divide o texto de acordo com a strig que foi selecionada nele e transformar em vetor a variavel
-var res=texto.split(" ");
+var res = texto.split(" ");
 //console.log(res[2]);
 
 //Indicar a posicao inicial e o tamanho do corte - Semelhante ao slice
-var res= texto.substr(0, 8);
+var res = texto.substr(0, 8);
 console.log(res);
 
 //Convertendo o texto para Mausculo e cortando o texto
-var res=texto.toUpperCase().substr(0,8);
+var res = texto.toUpperCase().substr(0, 8);
 
 //Minusculo
 // var res = texto.toLowerCase();
--- Course/50_Game_Forca.js
+++ Course/50_Game_Forca.js
@@ -9,5 +9,34 @@ desenhe os braços a cabeça que simula o jogo da forca
 // Criando um lista de palavras
 var list_word = ["Self", "This", "Morning", "Afertnoon", "Shipping"]
 
-console.log(list_word)
-// Em execução
+function raffling() {
+  return list_word[Math.round(Math.random() * (list_word.length - 1))].toLowerCase()
+}
+var word = raffling()
+document.querySelector("p").innerHTML = word
+
+
+var content_erros = document.querySelector(".erros")
+var input = document.getElementById("inp")
+var btn = document.getElementById("sub")
+
+// input.value = "Talismar"
+
+function forca() {
+  // Verificar se o Entry esta preenchido
+
+  if (word.includes(input.value)) {
+    console.log(input.value)
+    input.value = ""
+  }
+
+  else {
+    content_erros.innerHTML += "-"
+    input.value = ""
+  }
+
+}
+
+// Falta concluir esse desafio
+
+btn.addEventListener("click", forca)
